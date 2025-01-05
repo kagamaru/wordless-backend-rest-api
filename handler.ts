d91879bbc7f88ca82892be19ae9b543937cf4ff6
kagamaru <01a17124@gmail.com>
2025-01-05 14:59:40 +0900

    :sparkles: ローカルで検証できるようにする

--- handler.ts
+++ handler.ts
@@ -39,29 +39,6 @@ app.get("/users/:userId", async (req: any, res: any) => {
     }
 });
 
-app.post("/users", async (req, res) => {
-    const { userId, name } = req.body;
-    if (typeof userId !== "string") {
-        res.status(400).json({ error: '"userId" must be a string' });
-    } else if (typeof name !== "string") {
-        res.status(400).json({ error: '"name" must be a string' });
-    }
-
-    const params = {
-        TableName: USERS_TABLE,
-        Item: { userId, name },
-    };
-
-    try {
-        const command = new PutCommand(params);
-        await docClient.send(command);
-        res.json({ userId, name });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Could not create user" });
-    }
-});
-
 app.use((_: any, res: any, __: any) => {
     return res.status(404).json({
         error: "Not Found",
