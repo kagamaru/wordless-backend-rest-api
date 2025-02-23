cc7d8fb35a581fe1064ec83314193a0c51071a59
kagamaru <01a17124@gmail.com>
2025-02-23 21:58:36 +0900

    :recycle: メソッドを共通化する

--- app/users/findUser.ts
+++ app/users/findUser.ts
@@ -1,63 +1,38 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
-
-const USERS_TABLE = process.env.USERS_TABLE;
-
-let client = new DynamoDBClient({
-    region: "us-west-2",
-    credentials: { accessKeyId: "FAKE", secretAccessKey: "FAKE" },
-    endpoint: "http://localhost:8000",
-});
-
-if (process.env.DEPLOY_ENV !== "offline") {
-    client = new DynamoDBClient({
-        region: "us-west-2",
-    });
-}
-
-const docClient = DynamoDBDocumentClient.from(client);
+import { envConfig } from "@/config";
+import {
+    createErrorResponse,
+    createResponse,
+    getItemFromDynamoDB,
+} from "@/utility";
 
 export const findUser = async (
     event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
     if (!event.pathParameters || !event.pathParameters.userId) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ error: "USE-01" }),
-        };
+        return createErrorResponse(400, {
+            error: "USE-01",
+        });
     }
 
-    const { userId } = event.pathParameters;
-
-    const params = {
-        TableName: USERS_TABLE,
-        Key: {
-            userId,
-        },
-    };
+    const pathParameterUserId = event.pathParameters.userId;
 
     try {
-        const command = new GetCommand(params);
-        const { Item } = await docClient.send(command);
+        const { userId, userName, userAvatarUrl } = await getItemFromDynamoDB(
+            envConfig.USERS_TABLE,
+            { userId: pathParameterUserId },
+        );
 
-        if (Item) {
-            const { userId, userName, userAvatarUrl } = Item;
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ userId, userName, userAvatarUrl }),
-            };
-        } else {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ error: "USE-02" }),
-            };
-        }
+        return createResponse({ userId, userName, userAvatarUrl });
     } catch (error) {
-        console.error("Error fetching user:", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "USE-03" }),
-        };
+        if (error === "cannot find error") {
+            return createErrorResponse(500, {
+                error: "USE-02",
+            });
+        }
+
+        return createErrorResponse(500, {
+            error: "USE-03",
+        });
     }
 };
---
+++ utility/getItemFromDynamoDB.ts
@@ -0,0 +1,25 @@
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { getDynamoDBClient } from "./getDynamoDBClient";
+
+const docClient = getDynamoDBClient();
+
+export async function getItemFromDynamoDB<T>(
+    tableName: string,
+    key: Record<string, string>,
+): Promise<Record<string, T>> {
+    try {
+        const result = await docClient.send(
+            new GetCommand({
+                TableName: tableName,
+                Key: key,
+            }),
+        );
+        if (result.Item) {
+            return result.Item;
+        } else {
+            throw "cannot find error";
+        }
+    } catch (error) {
+        throw error;
+    }
+}
