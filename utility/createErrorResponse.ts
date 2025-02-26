bca1265bdea7019de70882ad4c6fa48cb0ec01e2
kagamaru <01a17124@gmail.com>
2025-02-26 22:46:45 +0900

    :sparkles: corsに関わる部分の設定をする

--- utility/createErrorResponse.ts
+++ utility/createErrorResponse.ts
@@ -1,10 +1,20 @@
 export function createErrorResponse(
     statusCode: 400 | 500,
     responseBody: { error: string },
+    originName: string,
 ) {
     console.error(responseBody.error);
     return {
         statusCode,
         body: JSON.stringify(responseBody),
+        headers: {
+            "Access-Control-Allow-Origin": originName.includes(
+                process.env.ALLOW_ORIGIN,
+            )
+                ? process.env.ALLOW_ORIGIN
+                : process.env.FRONTEND_URL,
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        },
     };
 }
--- utility/createResponse.ts
+++ utility/createResponse.ts
@@ -1,6 +1,15 @@
-export function createResponse(responseBody: Object) {
+export function createResponse(responseBody: Object, originName: string) {
     return {
         statusCode: 200,
         body: JSON.stringify(responseBody),
+        headers: {
+            "Access-Control-Allow-Origin": originName.includes(
+                process.env.ALLOW_ORIGIN,
+            )
+                ? process.env.ALLOW_ORIGIN
+                : process.env.FRONTEND_URL,
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        },
     };
 }
