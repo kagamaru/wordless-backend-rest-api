641483462bc6710284fcaf80e81f699c037f87ca
kagamaru <01a17124@gmail.com>
2025-02-26 23:05:29 +0900

    :recycle: AIレビューを受けての修正

--- utility/createResponse.ts
+++ utility/createResponse.ts
@@ -1,13 +1,23 @@
-export function createResponse(responseBody: Object, originName: string) {
+export function createResponse(
+    responseBody: Object,
+    originName: string,
+): {
+    statusCode: 200;
+    body: string;
+    headers: {
+        "Access-Control-Allow-Origin": string;
+        "Access-Control-Allow-Headers": "Content-Type";
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET";
+    };
+} {
     return {
         statusCode: 200,
         body: JSON.stringify(responseBody),
         headers: {
-            "Access-Control-Allow-Origin": originName.includes(
-                process.env.ALLOW_ORIGIN,
-            )
-                ? process.env.ALLOW_ORIGIN
-                : process.env.FRONTEND_URL,
+            "Access-Control-Allow-Origin":
+                originName === process.env.ALLOW_ORIGIN
+                    ? process.env.ALLOW_ORIGIN
+                    : process.env.FRONTEND_URL,
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
         },
