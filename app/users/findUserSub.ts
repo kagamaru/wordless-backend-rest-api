8928999a0fda2ef5c9992ea7e70433e6aaec39fc
kagamaru <01a17124@gmail.com>
2025-06-08 17:06:57 +0900

    :sparkles: UserSub取得APIの実装

---
+++ app/users/findUserSub.ts
@@ -0,0 +1,66 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { envConfig } from "@/config";
+import {
+    createErrorResponse,
+    createResponse,
+    getItemFromDynamoDB,
+} from "@/utility";
+
+const handleErrorResponse = (errorCode: string, originName: string) => {
+    const errorMap = {
+        "USE-12": 404,
+        "USE-13": 500,
+        "USE-14": 404,
+        "USE-15": 500,
+    };
+    return createErrorResponse(
+        errorMap[errorCode],
+        { error: errorCode },
+        originName,
+    );
+};
+
+export const findUserSub = async (
+    event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+    const originName = event.headers.origin;
+    if (!event.pathParameters || !event.pathParameters.userSub) {
+        return createErrorResponse(
+            400,
+            {
+                error: "USE-11",
+            },
+            originName,
+        );
+    }
+
+    const userSub = event.pathParameters.userSub;
+    let userIdFetchedFromUserSub: string;
+
+    try {
+        userIdFetchedFromUserSub = (
+            await getItemFromDynamoDB(envConfig.USER_SUB_TABLE, {
+                userSub,
+            })
+        ).userId as string;
+    } catch (error) {
+        return handleErrorResponse(
+            error.message === "Cannot find item" ? "USE-12" : "USE-13",
+            originName,
+        );
+    }
+
+    try {
+        const { userId, userName, userAvatarUrl } = await getItemFromDynamoDB(
+            envConfig.USERS_TABLE,
+            { userId: userIdFetchedFromUserSub },
+        );
+
+        return createResponse({ userId, userName, userAvatarUrl }, originName);
+    } catch (error) {
+        return handleErrorResponse(
+            error.message === "Cannot find item" ? "USE-14" : "USE-15",
+            originName,
+        );
+    }
+};
--- utility/createResponse.ts
+++ utility/createResponse.ts
@@ -3,12 +3,10 @@ import { envConfig } from "@/config";
 export function createResponse(
     responseBody: Object,
     originName: string,
-    token?: string,
 ): {
     statusCode: 200;
     body: string;
     headers: {
-        "Set-Cookie": string;
         "Access-Control-Allow-Origin": string;
         "Access-Control-Allow-Credentials": true;
         "Access-Control-Allow-Headers": "Content-Type";
@@ -19,7 +17,6 @@ export function createResponse(
         statusCode: 200,
         body: JSON.stringify(responseBody),
         headers: {
-            "Set-Cookie": token ?? undefined,
             "Access-Control-Allow-Origin":
                 originName === envConfig.ALLOW_ORIGIN
                     ? envConfig.ALLOW_ORIGIN
