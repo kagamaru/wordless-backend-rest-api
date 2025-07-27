6b8a23cbb1f48541fbe1f77c3c192e739c32922b
kagamaru <01a17124@gmail.com>
2025-07-27 18:48:37 +0900

    :sparkles: フォロー登録APIの実装

---
+++ app/follow/postFollow.ts
@@ -0,0 +1,114 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import {
+    createResponse,
+    createErrorResponse,
+    getRDSDBClient,
+    getItemFromDynamoDB,
+} from "@/utility";
+import { envConfig } from "@/config";
+
+const mysqlClient = getRDSDBClient();
+
+const handleDynamoDBError = (error: Error, originName: string) => {
+    if (error.message === "Cannot find item") {
+        return createErrorResponse(
+            404,
+            {
+                error: "FOL-13",
+            },
+            originName,
+        );
+    }
+    return createErrorResponse(
+        500,
+        {
+            error: "FOL-14",
+        },
+        originName,
+    );
+};
+
+export const postFollow = async (
+    event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+    const originName = event.headers.origin;
+    if (!event.body || !event.pathParameters) {
+        return createErrorResponse(
+            400,
+            {
+                error: "FOL-11",
+            },
+            originName,
+        );
+    }
+
+    const { followerId } = JSON.parse(event.body);
+    const { userId } = event.pathParameters;
+    const followeeId = userId;
+
+    if (!followerId || !followeeId || followerId === followeeId) {
+        return createErrorResponse(
+            400,
+            {
+                error: "FOL-12",
+            },
+            originName,
+        );
+    }
+
+    const dynamoPromises = [
+        getItemFromDynamoDB(envConfig.USERS_TABLE, { userId: followerId }),
+        getItemFromDynamoDB(envConfig.USERS_TABLE, { userId: followeeId }),
+    ];
+
+    // NOTE: フォローされるユーザーが存在しない場合、エラーを返す
+    // NOTE: フォローするユーザーが存在しない場合、エラーを返す
+    try {
+        await Promise.all(dynamoPromises);
+    } catch (error) {
+        if (error instanceof Error) {
+            return handleDynamoDBError(error, originName);
+        }
+    }
+
+    let followingArray: Array<{ followee_id: string }> = [];
+    let followeeArray: Array<{ follower_id: string }> = [];
+
+    try {
+        await mysqlClient.query(
+            `INSERT INTO wordlessdb.follow_table (follower_id, followee_id) VALUES (?, ?)`,
+            [followerId, followeeId],
+        );
+
+        followingArray = await mysqlClient.query(
+            `SELECT followee_id FROM wordlessdb.follow_table WHERE follower_id = ?`,
+            [followeeId],
+        );
+        followeeArray = await mysqlClient.query(
+            `SELECT follower_id FROM wordlessdb.follow_table WHERE followee_id = ?`,
+            [followeeId],
+        );
+
+        return createResponse(
+            {
+                totalNumberOfFollowing: followingArray.length,
+                followingUserIds: followingArray.map(
+                    (item) => item.followee_id,
+                ),
+                totalNumberOfFollowees: followeeArray.length,
+                followeeUserIds: followeeArray.map((item) => item.follower_id),
+            },
+            originName,
+        );
+    } catch (error) {
+        return createErrorResponse(
+            500,
+            {
+                error: "FOL-15",
+            },
+            originName,
+        );
+    } finally {
+        await mysqlClient.end();
+    }
+};
