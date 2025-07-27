f1d499790642522a8fb7359ce0bb55a1fbc74773
kagamaru <01a17124@gmail.com>
2025-07-27 12:13:50 +0900

    ✨ フォロー情報を取得するAPIを実装

---
+++ app/follow/fetchFollow.ts
@@ -0,0 +1,67 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { createResponse, createErrorResponse, getRDSDBClient } from "@/utility";
+
+const mysqlClient = getRDSDBClient();
+
+export const fetchFollow = async (
+    event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+    const originName = event.headers.origin;
+    if (!event.queryStringParameters) {
+        return createErrorResponse(
+            400,
+            {
+                error: "FOL-01",
+            },
+            originName,
+        );
+    }
+
+    const { userId } = event.queryStringParameters;
+
+    if (!userId) {
+        return createErrorResponse(
+            400,
+            {
+                error: "FOL-02",
+            },
+            originName,
+        );
+    }
+
+    // NOTE: 存在しないuserIdを指定したとしても、空配列が返却されるだけなので、userIdの実在性検査はしない
+
+    let followingArray: Array<string> = [];
+    let followeeArray: Array<string> = [];
+
+    try {
+        followingArray = await mysqlClient.query(
+            `SELECT followee_id FROM wordlessdb.follow_table WHERE follower_id = ?`,
+            [userId],
+        );
+        followeeArray = await mysqlClient.query(
+            `SELECT follower_id FROM wordlessdb.follow_table WHERE followee_id = ?`,
+            [userId],
+        );
+    } catch (error) {
+        return createErrorResponse(
+            500,
+            {
+                error: "FOL-03",
+            },
+            originName,
+        );
+    } finally {
+        await mysqlClient.end();
+    }
+
+    return createResponse(
+        {
+            totalNumberOfFollowing: followingArray.length,
+            followingUserIds: followingArray,
+            totalNumberOfFollowees: followeeArray.length,
+            followeeUserIds: followeeArray,
+        },
+        originName,
+    );
+};
