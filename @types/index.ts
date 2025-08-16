680276572bc5e08d96f81a21f4036ce8d3e767f5
kagamaru <01a17124@gmail.com>
2025-08-16 16:30:04 +0900

    :sparkles: ユーザー名登録APIの新規実装

--- @types/index.ts
+++ @types/index.ts
@@ -3,4 +3,5 @@ export * from "@/@types/ErrorCode";
 export * from "@/@types/FetchedEmote";
 export * from "@/@types/GetUserRequest";
 export * from "@/@types/PostUserSukiPayload";
+export * from "@/@types/User";
 export * from "@/@types/UserSukiEmojis";
